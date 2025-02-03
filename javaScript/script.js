85387f66499194431e5e91b60664cc5251bb2b8e
Ameen-hp <devameen45@gmail.com>
2025-02-03 09:41:31 -0800

    javaScript course code

---
+++ javaScript/script.js
@@ -0,0 +1,70 @@
+// let,const,var
+
+// let
+
+// let store = 123;
+// console.log(store);
+
+// store = 12345;
+// console.log(store);
+
+
+// const
+
+// const store = 123;
+// console.log(store);
+// store = 12345;
+// console.log(store);
+
+
+// var
+
+// var store = 123;
+// console.log(store)
+// store = 12324;
+// console.log(store)
+// var store = 123323;
+// console.log(store);
+
+// operetors    operands
+
+// +,-,*
+
+// let addition = 3 + 5;
+
+
+// Arithematic operators
+// +,-,*,/,%,**,--,++
+// let x = 3;
+// let y = 4;
+// console.log(x + y+" addition");
+// console.log(x - y+" subtraction");
+// console.log(x * y+" multiplication");
+// console.log(x / y+" division");
+// console.log(x ** y+" square");
+// console.log(x % y+" module");
+// console.log(++x +" increment");
+// console.log(--x + " decrement");
+
+// let z = 2;
+// ++z;  // increment  2+1; z = 3
+// console.log(z);
+// --z   // decrement  3-1 = 2
+// console.log(z);
+
+
+// assigment operators
+// =,+=,-=,*=,/=,%=,**=,--=,++=
+
+// let y = 3;
+// let z = 2;
+// // z += y;  // z = z+y;
+// z -= y;    //  z = z - y == 2 - 3 
+
+// console.log(`-= ${z}`);  // -1
+// z+=y
+// console.log(`+=   ${z}` )
+
+
+
+
